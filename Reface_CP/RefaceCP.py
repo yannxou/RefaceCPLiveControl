2814b79a3f286169a48a282a98bef1a2d68888bf
Joan Duat <yannxou@gmail.com>
2024-08-18 11:52:17 +0200

    set midi channel based on type value

--- Reface_CP/RefaceCP.py
+++ Reface_CP/RefaceCP.py
@@ -12,9 +12,22 @@ from _Framework.DeviceComponent import DeviceComponent
 from ableton.v2.base import listens, liveobj_valid, liveobj_changed
 #from _Framework.ChannelStripComponent import ChannelStripComponent
 
-# Reface CP elements:
+# Reface CP MIDI CCs:
 TYPE_SELECT_KNOB = 80
 ENCODER_MSG_IDS = (81, 18, 19, 86, 87, 89, 90, 91) # Reface CP knobs from Drive to Reverb Depth
+TRMOLO_WAH_SWITCH = 17
+CHORUS_PHASER_SWITCH = 85
+DELAY_SWITCH = 88
+
+# Reface type knob CC value to MIDI channel map
+reface_type_map = {
+    0: 0,
+    25: 1,
+    51: 2,
+    76: 3,
+    102: 4,
+    127: 5
+}
 
 class RefaceCP(ControlSurface):
     def __init__(self, c_instance):
@@ -32,9 +45,14 @@ class RefaceCP(ControlSurface):
 
             self.log_message("RefaceCP Init Succeeded.")
 
+# --- Setup
+
     def _setup_buttons(self):
-        self.type_select_button = ButtonElement(1, MIDI_CC_TYPE, 0, TYPE_SELECT_KNOB)
-        self.type_select_button.add_value_listener(self._type_select_changed)
+        self._type_select_buttons = []
+        for index in range(len(reface_type_map)):
+            button = ButtonElement(1, MIDI_CC_TYPE, index, TYPE_SELECT_KNOB)
+            button.add_value_listener(self._reface_type_select_changed)
+            self._type_select_buttons.append(button)
 
     def _setup_device_control(self):
         self._device = DeviceComponent()
@@ -48,6 +66,8 @@ class RefaceCP(ControlSurface):
         self._on_device_changed.subject = self._device
         self.set_device_component(self._device)
 
+# --- Listeners
+
     @subject_slot('device')
     def _on_device_changed(self):
         if liveobj_valid(self._device):
@@ -56,26 +76,30 @@ class RefaceCP(ControlSurface):
             # no device
             self.log_message("No device selected")
 
-    def _type_select_changed(self, value):
-        self.log_message(f"Type changed: {value}")
+    def _reface_type_select_changed(self, value):
+        channel = reface_type_map.get(value, 0)
+        self.log_message(f"Type changed: {value} -> {channel}")
+        self._setRefaceTransmitChannel(channel)
+
+# --- Reface Sysex commands
+# Specs: https://usa.yamaha.com/files/download/other_assets/7/794817/reface_en_dl_b0.pdf
+
+    def _setRefaceTransmitChannel(self, channel):
+        sysex_start = 0xF0  # SysEx message start
+        sysex_end = 0xF7  # SysEx message end
+        device_id = 0x43 # Yamaha ID
+        device_number = 0x10 # Device Number
+        group_high = 0x7F # Group number high
+        group_low = 0x1C # Group number low
+        model_id = 0x04 # Model ID
+        sys_ex_message = (sysex_start, device_id, device_number, group_high, group_low, model_id, 0x00, 0x00, 0x00, channel, sysex_end)
+        self._send_midi(sys_ex_message)
+
 
     def disconnect(self):
         self.log_message("RefaceCP Disconnected")
-        self.type_select_button.remove_value_listener(self._type_select_changed)
+        for button in self._type_select_buttons:
+            button.remove_value_listener(self._reface_type_select_changed)
+        self._type_select_buttons = []
 
         super(RefaceCP, self).disconnect()
-
-"""
-        # Initialize a simple button
-        is_momentary = True
-        channel = 0
-        note = 60  # Middle C
-
-        self.my_button = ButtonElement(is_momentary, MIDI_NOTE_TYPE, channel, note)
-        self.my_button.add_value_listener(self.button_pressed)
-
-    def button_pressed(self, value):
-        if value > 0:  # Button pressed
-            self.log_message("Button Pressed")
-            self.show_message("Hello from MyController")
-"""
