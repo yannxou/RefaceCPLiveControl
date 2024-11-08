f14e6644634f3022f9dfb9429438361375f28aa5
Joan Duat <yannxou@gmail.com>
2024-11-08 12:16:54 +0100

    select between scales from matching notes in edit mode

--- Reface_CP/Note.py
+++ Reface_CP/Note.py
@@ -34,6 +34,8 @@ class Note:
         11: 6  # B
     }
 
+    NOTE_NAMES = {0: "C", 1: "C#", 2: "D", 3: "D#", 4: "E", 5: "F", 6: "F#", 7: "G", 8: "G#", 9: "A", 10: "A#", 11: "B"}
+
     @staticmethod
     def is_black_key(note):
         # Notes corresponding to black keys in an octave
--- Reface_CP/ScaleModeController.py
+++ Reface_CP/ScaleModeController.py
@@ -15,6 +15,7 @@ import Live.Song
 from _Framework.ButtonElement import ButtonElement
 from _Framework.InputControlElement import MIDI_NOTE_TYPE
 from .Logger import Logger
+from .Note import Note
 
 class ScaleModeController:
     
@@ -40,6 +41,8 @@ class ScaleModeController:
         self._on_note_event = on_note_event
         self._note_key_buttons = []
         self._pressed_keys = []
+        self._captured_notes = set()  # Set of pitches (0..11) captured during scale edit
+        self._custom_matching_scales = []
         self._current_root_note = -1
         self._current_scale_intervals = None
         self._all_scales = Live.Song.get_all_scales_ordered()
@@ -148,12 +151,21 @@ class ScaleModeController:
             self._edit_mode_button.remove_value_listener(self._on_edit_mode_button_changed)
 
     def _on_root_note_button_changed(self, value):
-        total_notes = 12
-        note = int((value / 127.0) * (total_notes - 1))
-        if note != self._song.root_note:
-            self._song.root_note = note
+        if self._edit_mode_enabled:
+            total_scales = len(self._custom_matching_scales)
+            if total_scales > 0:
+                scale_index = int((value / 127.0) * (total_scales - 1))
+                self._song.root_note = self._custom_matching_scales[scale_index][0]
+                self._song.scale_name = self._custom_matching_scales[scale_index][1]
+        else:
+            total_notes = 12
+            note = int((value / 127.0) * (total_notes - 1))
+            if note != self._song.root_note:
+                self._song.root_note = note
 
     def _on_scale_mode_button_changed(self, value):
+        if self._edit_mode_enabled:
+            return
         total_scales = len(self._all_scales)
         scale_index = int((value / 127.0) * (total_scales - 1))
         self._song.scale_name = self._all_scales[scale_index][0]
@@ -188,10 +200,34 @@ class ScaleModeController:
             self._pressed_keys.append(key)
             if len(self._pressed_keys) == 1:
                 self._song.root_note = key % 12
+                self._captured_notes = set()
+                # self._logger.log(f"Set root {key % 12}")
+            self._captured_notes.add(key % 12)
         else:
             self._pressed_keys.remove(key)
+            if len(self._pressed_keys) == 0:
+                self._custom_matching_scales = self._find_scales(self._captured_notes, starting_root=self._current_root_note)
+                self._song.root_note = self._custom_matching_scales[0][0]
+                self._song.scale_name = self._custom_matching_scales[0][1]
+                note_names = [Note.NOTE_NAMES[note] for note in sorted(self._captured_notes)]
+                self._logger.show_message(f"Found {len(self._custom_matching_scales)} scales including notes {note_names}")
         # self._logger.log(f"Note: {key}, velocity: {value}")
         self._on_note_event(key, value)
+
+    def _find_scales(self, notes: set, starting_root = 0):
+        """Returns a list of tuples of (root, scale) with scales that include all the given notes. List is sorted from the starting_root and upwards"""
+        matching_scales = []
+        for root_index in range(12):
+            root = (starting_root + root_index) % 12
+            for scale in self._all_scales:
+                scale_name = scale[0]
+                original_intervals = scale[1]
+                transposed_intervals = {(root + interval) % 12 for interval in original_intervals}
+                # Check if all notes in the note_pitch_classes are in the transposed_intervals
+                if notes.issubset(transposed_intervals):
+                    self._logger.log(f"Match: {scale_name} in {root}")
+                    matching_scales.append((root, scale_name))
+        return matching_scales
 
     def disconnect(self):
         self._remove_button_listeners()
