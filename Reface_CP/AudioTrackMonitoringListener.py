4bd1dc2dbe5846e9be52303939eb9f73c46198a9
Joan Duat <yannxou@gmail.com>
2024-11-16 14:08:51 +0100

    disable script also when track monitoring state is set to IN

--- Reface_CP/AudioTrackMonitoringListener.py
+++ Reface_CP/AudioTrackMonitoringListener.py
@@ -9,6 +9,7 @@
 #
 # Distributed under the MIT License, see LICENSE
 
+import Live.Track
 from .Logger import Logger
 import Live.Song
 import re
@@ -45,6 +46,7 @@ class AudioTrackMonitoringListener:
         self._on_monitoring_changed = on_monitoring_changed
         self._track_name_listeners = {}
         self._track_arm_listeners = {}
+        self._track_monitoring_listeners = {}
         self._last_sent_value = False
         all_tracks = get_all_tracks(self._song)
         self._song.add_tracks_listener(self._on_tracks_changed)
@@ -52,11 +54,11 @@ class AudioTrackMonitoringListener:
             if track.has_audio_input:
                 self._add_track_name_listener(track)
                 self._update_monitoring_listeners(track)
-        self._check_monitoring_status()
+        self._check_matching_tracks()
 
-    def _add_track_name_listener(self, track):
+    def _add_track_name_listener(self, track: Live.Track.Track):
         if track._live_ptr not in self._track_name_listeners:
-            self._logger.log(f"Adding name listener to track: {track.name}")
+            # self._logger.log(f"Adding name listener to track: {track.name}")
             listener = partial(self._on_track_name_changed, track)
             self._track_name_listeners[track._live_ptr] = listener
             track.add_name_listener(listener)
@@ -75,7 +77,7 @@ class AudioTrackMonitoringListener:
             finally:
                 del self._track_name_listeners[live_ptr]
 
-    def _add_track_arm_listener(self, track):
+    def _add_track_arm_listener(self, track: Live.Track.Track):
         if track._live_ptr not in self._track_arm_listeners:
             # self._logger.log(f"Adding arm listener to track: {track.name}")
             listener = partial(self._on_track_arm_changed, track)
@@ -96,6 +98,27 @@ class AudioTrackMonitoringListener:
             finally:
                 del self._track_arm_listeners[live_ptr]
 
+    def _add_track_monitoring_listener(self, track: Live.Track.Track):
+        if track._live_ptr not in self._track_monitoring_listeners:
+            # self._logger.log(f"Adding monitoring listener to track: {track.name}")
+            listener = partial(self._on_track_monitoring_changed, track)
+            self._track_monitoring_listeners[track._live_ptr] = listener
+            track.add_current_monitoring_state_listener(listener)
+
+    def _remove_track_monitoring_listener(self, live_ptr):
+        if live_ptr in self._track_monitoring_listeners:
+            # self._logger.log(f"Removing monitoring listener from track: {live_ptr}")
+            listener = self._track_monitoring_listeners[live_ptr]
+            try:
+                # Find the track object using the _live_ptr
+                track = next((t for t in self._song.tracks if t._live_ptr == live_ptr), None)
+                if track and track.current_monitoring_state_has_listener(listener):
+                    track.remove_current_monitoring_state_listener(listener)
+            except:
+                pass
+            finally:
+                del self._track_monitoring_listeners[live_ptr]
+
     def _on_tracks_changed(self):
         """Listener function called when a track is added or deleted"""
         current_tracks = {track._live_ptr: track for track in self._song.tracks}
@@ -106,6 +129,7 @@ class AudioTrackMonitoringListener:
         for live_ptr in removed_tracks:
             self._remove_track_name_listener(live_ptr)
             self._remove_track_arm_listener(live_ptr)
+            self._remove_track_monitoring_listener(live_ptr)
 
         # Tracks that have been added
         added_tracks = current_tracks.keys() - previous_tracks
@@ -115,40 +139,45 @@ class AudioTrackMonitoringListener:
                 self._add_track_name_listener(track)
                 self._update_monitoring_listeners(track)
         
-        self._check_monitoring_status()
+        self._check_matching_tracks()
 
-    def _on_track_arm_changed(self, track):
+    def _on_track_arm_changed(self, track: Live.Track.Track):
         # self._logger.log(f"_on_arm_changed: {track.name}, {track.arm}")
-        bypass = True if track.arm else False
-        if self._on_monitoring_changed and bypass != self._last_sent_value:
-            self._on_monitoring_changed(bypass)
-            self._last_sent_value = bypass
+        self._check_track_monitoring_bypass(track)
 
-    def _on_track_name_changed(self, track):
+    def _on_track_monitoring_changed(self, track: Live.Track.Track):
+        # self._logger.log(f"_on_track_monitoring_changed: {track.name}, {track.current_monitoring_state}")
+        self._check_track_monitoring_bypass(track)
+
+    def _on_track_name_changed(self, track: Live.Track.Track):
         # self._logger.log(f"_on_track_name_changed: {track.name}")
         self._update_monitoring_listeners(track)
+        self._check_matching_tracks()
 
-    def _update_monitoring_listeners(self, track):
+    def _update_monitoring_listeners(self, track: Live.Track.Track):
         if track.has_audio_input:
             if re.search(self._track_name_pattern, track.name, re.IGNORECASE):
-                self._logger.log(f"Track match: {track.name}")
+                # self._logger.log(f"Track match: {track.name}")
                 self._add_track_arm_listener(track)
+                self._add_track_monitoring_listener(track)
             else:
                 self._remove_track_arm_listener(track)
+                self._remove_track_monitoring_listener(track)
     
-    def _check_monitoring_status(self):
-        bypass = self._last_sent_value
-        enabled_track = next((t for t in self._song.tracks if t.has_audio_input and re.search(self._track_name_pattern, t.name, re.IGNORECASE) and t.arm), None)
-        if enabled_track:
-            if self._last_sent_value == False:
-                bypass = True
-        else:
-            if self._last_sent_value == True:
-                bypass = False
+    def _check_matching_tracks(self):
+        bypass = next((t for t in self._song.tracks if re.search(self._track_name_pattern, t.name, re.IGNORECASE) and self._is_track_monitoring_enabled(t)), None)
         if self._on_monitoring_changed and bypass != self._last_sent_value:
             self._on_monitoring_changed(bypass)
             self._last_sent_value = bypass
 
+    def _check_track_monitoring_bypass(self, track: Live.Track.Track):
+        bypass = True if self._is_track_monitoring_enabled(track) else False
+        if self._on_monitoring_changed and bypass != self._last_sent_value:
+            self._on_monitoring_changed(bypass)
+            self._last_sent_value = bypass
+
+    def _is_track_monitoring_enabled(self, track: Live.Track.Track):
+        return track.has_audio_input and (track.arm or track.current_monitoring_state == track.monitoring_states.IN)
 
     def disconnect(self):
         for live_ptr in list(self._track_name_listeners.keys()):
